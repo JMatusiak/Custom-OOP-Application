683ce68b272fb2feaa7e223af9566dfd40a5c51c
JMatusiak <115078341+JMatusiak@users.noreply.github.com>
2024-01-14 22:45:16 +0100

    Add files via upload

---
+++ Projekt/MenuStrip/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace MenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewTaskStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.TasksStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddTaskStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditTaskStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.DeleteTaskStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListBoxTasks = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileStripMenu,
+            this.TasksStripMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileStripMenu
+            // 
+            this.FileStripMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.FileStripMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewTaskStripMenu,
+            this.ExitStripMenu,
+            this.AboutStripMenu});
+            this.FileStripMenu.Name = "FileStripMenu";
+            this.FileStripMenu.Size = new System.Drawing.Size(37, 20);
+            this.FileStripMenu.Text = "File";
+            // 
+            // NewTaskStripMenu
+            // 
+            this.NewTaskStripMenu.Name = "NewTaskStripMenu";
+            this.NewTaskStripMenu.Size = new System.Drawing.Size(123, 22);
+            this.NewTaskStripMenu.Text = "New Task";
+            this.NewTaskStripMenu.Click += new System.EventHandler(this.NewTaskStripMenu_Click);
+            // 
+            // ExitStripMenu
+            // 
+            this.ExitStripMenu.Name = "ExitStripMenu";
+            this.ExitStripMenu.Size = new System.Drawing.Size(123, 22);
+            this.ExitStripMenu.Text = "Exit";
+            // 
+            // AboutStripMenu
+            // 
+            this.AboutStripMenu.Name = "AboutStripMenu";
+            this.AboutStripMenu.Size = new System.Drawing.Size(123, 22);
+            this.AboutStripMenu.Text = "About";
+            this.AboutStripMenu.Click += new System.EventHandler(this.AboutStripMenu_Click);
+            // 
+            // TasksStripMenu
+            // 
+            this.TasksStripMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.TasksStripMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddTaskStripMenu,
+            this.EditTaskStripMenu,
+            this.DeleteTaskStripMenu});
+            this.TasksStripMenu.Name = "TasksStripMenu";
+            this.TasksStripMenu.Size = new System.Drawing.Size(46, 20);
+            this.TasksStripMenu.Text = "Tasks";
+            // 
+            // AddTaskStripMenu
+            // 
+            this.AddTaskStripMenu.Name = "AddTaskStripMenu";
+            this.AddTaskStripMenu.Size = new System.Drawing.Size(132, 22);
+            this.AddTaskStripMenu.Text = "Add Task";
+            this.AddTaskStripMenu.Click += new System.EventHandler(this.AddTaskStripMenu_Click);
+            // 
+            // EditTaskStripMenu
+            // 
+            this.EditTaskStripMenu.Name = "EditTaskStripMenu";
+            this.EditTaskStripMenu.Size = new System.Drawing.Size(132, 22);
+            this.EditTaskStripMenu.Text = "Edit Task";
+            this.EditTaskStripMenu.Click += new System.EventHandler(this.EditTaskStripMenu_Click);
+            // 
+            // DeleteTaskStripMenu
+            // 
+            this.DeleteTaskStripMenu.Name = "DeleteTaskStripMenu";
+            this.DeleteTaskStripMenu.Size = new System.Drawing.Size(132, 22);
+            this.DeleteTaskStripMenu.Text = "Delete Task";
+            this.DeleteTaskStripMenu.Click += new System.EventHandler(this.DeleteTaskStripMenu_Click);
+            // 
+            // ListBoxTasks
+            // 
+            this.ListBoxTasks.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ListBoxTasks.FormattingEnabled = true;
+            this.ListBoxTasks.Location = new System.Drawing.Point(33, 50);
+            this.ListBoxTasks.Name = "ListBoxTasks";
+            this.ListBoxTasks.Size = new System.Drawing.Size(704, 355);
+            this.ListBoxTasks.TabIndex = 1;
+            this.ListBoxTasks.SelectedIndexChanged += new System.EventHandler(this.ListBoxTasks_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListBoxTasks);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem NewTaskStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem ExitStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem TasksStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem AddTaskStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem EditTaskStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem DeleteTaskStripMenu;
+        private System.Windows.Forms.ListBox ListBoxTasks;
+        private System.Windows.Forms.ToolStripMenuItem AboutStripMenu;
+    }
+}
+
---
+++ Projekt/MenuStrip/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace MenuStrip
+{
+    public partial class Form1 : Form
+    {
+        private const string Filename = "tasks.json";
+
+        private List<Task> tasks = new List<Task>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+        private void NewTaskStripMenu_Click(object sender, EventArgs e)
+        {
+            CreateNewTask();
+            RefreshTaskList();
+            SaveTasks();
+        }
+
+        private void AddTaskStripMenu_Click(object sender, EventArgs e)
+        {
+            CreateNewTask();
+            RefreshTaskList();
+            SaveTasks();
+        }
+
+        private void ListBoxTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool isTaskSelected = ListBoxTasks.SelectedIndex != -1;
+            EditTaskStripMenu.Enabled = isTaskSelected;
+            DeleteTaskStripMenu.Enabled = isTaskSelected;
+        }
+
+        private void EditTaskStripMenu_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = ListBoxTasks.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                Task selectedTask = tasks[selectedIndex];
+                EditTask(selectedTask);
+                RefreshTaskList();
+                SaveTasks();
+            }
+        }
+
+        private void DeleteTaskStripMenu_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = ListBoxTasks.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                tasks.RemoveAt(selectedIndex);
+                RefreshTaskList();
+                SaveTasks();
+            }
+        }
+
+        private void RefreshTaskList()
+        {
+            tasks.Sort();
+            ListBoxTasks.Items.Clear();
+
+            foreach (Task task in tasks)
+            {
+                ListBoxTasks.Items.Add($"Opis: {task.Description}, Data: {task.Date}, Priorytet: {task.Priority}");
+            }
+        }
+
+        private void CreateNewTask()
+        {
+            Label descriptionLabel = new Label();
+            TextBox descriptionTextBox = new TextBox();
+
+            Label dateLabel = new Label();
+            DateTimePicker datePicker = new DateTimePicker();
+
+            Label priorityLabel = new Label();
+            ComboBox priorityComboBox = new ComboBox();
+
+            descriptionLabel.Text = "Opis";
+            descriptionTextBox.Dock = DockStyle.Top;
+
+            descriptionTextBox.Dock = DockStyle.Top;
+
+            dateLabel.Text = "Data";
+            datePicker.Dock = DockStyle.Top;
+            datePicker.Format = DateTimePickerFormat.Custom;
+            datePicker.CustomFormat = "dd.MM.yyyy HH:mm";
+
+            priorityLabel.Text = "Priorytet";
+            priorityComboBox.Items.AddRange(new object[] { "Wysoki", "Średni", "Niski" });
+            priorityComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            priorityComboBox.Dock = DockStyle.Top;
+
+            Form form = new Form();
+            form.Text = "Nowe zadanie";
+            form.AutoSize = true;
+
+            form.Controls.Add(descriptionLabel);
+            form.Controls.Add(descriptionTextBox);
+
+            form.Controls.Add(dateLabel);
+            form.Controls.Add(datePicker);
+
+            form.Controls.Add(priorityLabel);
+            form.Controls.Add(priorityComboBox);
+
+            descriptionLabel.Dock = DockStyle.Top;
+            dateLabel.Dock = DockStyle.Top;
+            priorityLabel.Dock = DockStyle.Top;
+
+            Button confirmButton = new Button();
+            confirmButton.Text = "Confirm";
+            confirmButton.Click += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(descriptionTextBox.Text) || priorityComboBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Wprowadź opis i wybierz priorytet.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Task task = new Task
+                {
+                    Description = descriptionTextBox.Text,
+                    Date = datePicker.Value,
+                    Priority = priorityComboBox.SelectedItem.ToString()
+                };
+                tasks.Add(task);
+
+                form.Close();
+            };
+
+            Button cancelButton = new Button();
+            cancelButton.Text = "Cancel";
+            cancelButton.Click += (sender, e) =>
+            {
+                form.Close();
+            };
+
+            confirmButton.Dock = DockStyle.Bottom;
+            cancelButton.Dock = DockStyle.Bottom;
+
+            form.Controls.Add(confirmButton);
+            form.Controls.Add(cancelButton);
+
+            form.ShowDialog();
+        }
+
+        private void EditTask(Task task)
+        {
+            Form editForm = new Form();
+            editForm.Text = "Edytuj zadanie";
+            editForm.AutoSize = true;
+
+            TextBox descriptionTextBox = new TextBox();
+            descriptionTextBox.Text = task.Description;
+
+            DateTimePicker datePicker = new DateTimePicker();
+            datePicker.Value = task.Date;
+            datePicker.Format = DateTimePickerFormat.Custom;
+            datePicker.CustomFormat = "dd.MM.yyyy HH:mm";
+
+            ComboBox priorityComboBox = new ComboBox();
+            priorityComboBox.Items.AddRange(new object[] { "Wysoki", "Średni", "Niski" });
+            priorityComboBox.SelectedItem = task.Priority;
+            priorityComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            Button confirmButton = new Button();
+            confirmButton.Text = "Confirm";
+            confirmButton.Click += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(descriptionTextBox.Text))
+                {
+                    MessageBox.Show("Wprowadź opis.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                task.Description = descriptionTextBox.Text;
+                task.Date = datePicker.Value;
+                task.Priority = priorityComboBox.SelectedItem.ToString();
+
+                RefreshTaskList();
+                editForm.Close();
+            };
+
+            Button cancelButton = new Button();
+            cancelButton.Text = "Cancel";
+            cancelButton.Click += (sender, e) =>
+            {
+                editForm.Close();
+            };
+
+            editForm.Controls.Add(descriptionTextBox);
+            editForm.Controls.Add(datePicker);
+            editForm.Controls.Add(priorityComboBox);
+            editForm.Controls.Add(confirmButton);
+            editForm.Controls.Add(cancelButton);
+
+            descriptionTextBox.Dock = DockStyle.Top;
+            datePicker.Dock = DockStyle.Top;
+            priorityComboBox.Dock = DockStyle.Top;
+            confirmButton.Dock = DockStyle.Bottom;
+            cancelButton.Dock = DockStyle.Bottom;
+
+            editForm.ShowDialog();
+        }
+
+        private void LoadTasks()
+        {
+            try
+            {
+                if (File.Exists(Filename))
+                {
+                    string json = File.ReadAllText(Filename);
+                    tasks = JsonConvert.DeserializeObject<List<Task>>(json);
+                    RefreshTaskList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd w trakcie wczytywania zadań!: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveTasks()
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                File.WriteAllText(Filename, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd w zapisywaniu zadań!: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTasks();
+        }
+
+        private void AboutStripMenu_Click(object sender, EventArgs e)
+        {
+            Form aboutDialog = new Form();
+            aboutDialog.Text = "O aplikacji";
+            aboutDialog.AutoSize = true;
+
+            Label creatorLabel = new Label();
+            creatorLabel.Text = "Twórca: Jakub Matusiak";
+            creatorLabel.Dock = DockStyle.Top;
+
+            Label versionLabel = new Label();
+            versionLabel.Text = $"Wersja .NET: {Environment.Version}";
+            versionLabel.Dock = DockStyle.Top;
+
+            Button closeButton = new Button();
+            closeButton.Text = "Zamknij";
+            closeButton.Click += (_, __) => aboutDialog.Close();
+            closeButton.Dock = DockStyle.Bottom;
+
+            aboutDialog.Controls.Add(creatorLabel);
+            aboutDialog.Controls.Add(versionLabel);
+            aboutDialog.Controls.Add(closeButton);
+            aboutDialog.ShowDialog();
+        }
+
+        private class Task : IComparable<Task>
+        {
+            public string Description { get; set; }
+            public DateTime Date { get; set; }
+            public string Priority { get; set; }
+
+            public int CompareTo(Task other)
+            {
+                if (other == null) return 1;
+
+                Dictionary<string, int> priorityOrder = new Dictionary<string, int>
+                {
+                     { "Wysoki", 0 },
+                     { "Średni", 1 },
+                     { "Niski", 2 }
+                };
+
+                return priorityOrder[Priority].CompareTo(priorityOrder[other.Priority]);
+            }
+        }
+    }
+}
